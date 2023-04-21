7986eed55dce17ec7483c1a75863010fc7d5bb8e
Aathiq <123265489+Aathiq-Ahamed@users.noreply.github.com>
2023-04-21 11:06:06 +0530

    sql changes

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "1291073129749013740932ABFG"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///boxit.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
